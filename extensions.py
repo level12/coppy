fef12a3ab99b999122362a73d946258147670c69
Randy Syring <randy@syrings.us>
2024-05-16 15:36:58 -0400

    add pre-commit

--- extensions.py
+++ extensions.py
@@ -6,31 +6,27 @@ from jinja2.ext import Extension
 
 
 def git_user_name() -> str:
-    return subprocess.getoutput("git config user.name").strip()
+    return subprocess.getoutput('git config user.name').strip()
 
 
 def git_user_email() -> str:
-    return subprocess.getoutput("git config user.email").strip()
+    return subprocess.getoutput('git config user.email').strip()
 
 
-def slugify(value, separator="-"):
-    value = (
-        unicodedata.normalize("NFKD", str(value))
-        .encode("ascii", "ignore")
-        .decode("ascii")
-    )
-    value = re.sub(r"[^\w\s-]", "", value.lower())
-    return re.sub(r"[-_\s]+", separator, value).strip("-_")
+def slugify(value, separator='-'):
+    value = unicodedata.normalize('NFKD', str(value)).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub(r'[^\w\s-]', '', value.lower())
+    return re.sub(r'[-_\s]+', separator, value).strip('-_')
 
 
 class GitExtension(Extension):
     def __init__(self, environment):
         super().__init__(environment)
-        environment.globals["git_user_name"] = git_user_name
-        environment.globals["git_user_email"] = git_user_email
+        environment.globals['git_user_name'] = git_user_name
+        environment.globals['git_user_email'] = git_user_email
 
 
 class SlugifyExtension(Extension):
     def __init__(self, environment):
         super().__init__(environment)
-        environment.filters["slugify"] = slugify
+        environment.filters['slugify'] = slugify
--- noxfile.py
+++ noxfile.py
@@ -22,3 +22,14 @@ def tests(session: nox.Session):
         '--strict-markers',
         f'--junit-xml={package_path}/ci/test-reports/{session.name}.pytests.xml',
     )
+
+
+@nox.session
+def standards(session: nox.Session):
+    session.install('-c', 'requirements/dev.txt', 'pre-commit')
+    session.run(
+        'pre-commit',
+        'run',
+        '--show-diff-on-failure',
+        '--all-files',
+    )
