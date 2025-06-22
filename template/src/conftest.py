3ba097d930d58d5057918844db3239096e21a996
Randy Syring <randy.syring@level12.io>
2025-06-21 20:33:46 -0400

    conftest.py: fix handling of warnings
    fixes #49

---
+++ template/src/conftest.py
@@ -0,0 +1,45 @@
+"""
+This conftest mostly for handling warnings.  Use the other conftest.py files for app/test config.
+
+- Filters for warnings that are triggered during import go at the top level.
+- Filters for warnings thrown during test runs goes in pytest_configure() below.
+"""
+
+import warnings
+
+
+# Treat any warning issued in a test as an exception so we are forced to explicitly handle or
+# ignore it.
+warnings.filterwarnings('error')
+# Examples:
+# warnings.filterwarnings(
+#     'ignore',
+#     "'cgi' is deprecated and slated for removal in Python 3.13",
+#     category=DeprecationWarning,
+#     module='webob.compat',
+# )
+# warnings.filterwarnings(
+#     'ignore',
+#     "'crypt' is deprecated and slated for removal in Python 3.13",
+#     category=DeprecationWarning,
+#     module='passlib.utils',
+# )
+###########
+# REMINDER: when adding an ignore, add an issue to track it
+###########
+
+
+def pytest_configure(config):
+    """
+    You may be able to do all your ignores above.  If you find some warnings need to be ignored
+    in pytest, you can do that with something like:
+
+        config.addinivalue_line(
+            'filterwarnings',
+            # Note the lines that follow are implicitly concatinated, no "," at the end
+            'ignore'
+            ':pythonjsonlogger.jsonlogger has been moved to pythonjsonlogger.json'
+            ':DeprecationWarning'
+            ':wtforms.meta',
+        )
+    """
