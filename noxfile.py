bfe2349a0e0102b0c9d1880922e55f608eef053c
Randy Syring <randy.syring@level12.io>
2025-09-16 13:10:06 -0400

    Fix coppy's nox runs

--- noxfile.py
+++ noxfile.py
@@ -7,19 +7,25 @@ package_path = Path.cwd()
 nox.options.default_venv_backend = 'uv'
 
 
-def uv_sync(session, group: str):
-    session.run('uv', 'sync', '--active', '--frozen', '--only-group', group)
+def uv_sync(session, group: str, project: bool = False):
+    # NOTE: not sure why, but sys.path doesn't get correctly configured to include the coppy pkg's
+    # `src` directory when using '--only-group'.  But, in a templated generated app, like the
+    # coppy demo, '--only-group' works just fine.  I spent some time trying to debug but am punting
+    # for now since the problem/solution seem convoluted to me.
+    groups_args = ('--no-default-groups', '--group') if project else ('--only-group',)
+    session.run('uv', 'sync', '--active', '--frozen', *groups_args, group)
 
 
 @nox.session
 def tests(session: nox.Session):
-    uv_sync(session, 'tests')
+    uv_sync(session, 'tests', project=True)
     session.run(
         'pytest',
         '-ra',
         '--tb=native',
         '--strict-markers',
         f'--junit-xml={package_path}/ci/test-reports/{session.name}.pytests.xml',
+        'src/tests',
         *session.posargs,
     )
 
